71f62ba3605bf3a3abb043d7e7fd8a71fcfc2f67
forever208 <ningmang666@gmail.com>
2025-09-16 10:53:13 +0200

    visualize recon

--- latent_analysis.py
+++ latent_analysis.py
@@ -82,8 +82,73 @@ def visualize_latent(path_to_pretrained_weights=None, config_file=None,
             plt.close()
 
 
+
+def visualize_recon(path_to_pretrained_weights=None, config_file=None,
+                    dataset=None, img_sz=None, path_to_dataset=None,):
+
+    print("visualize image latents for:", dataset)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    ### Load VAE Config ###
+    with open(config_file, "r") as f:
+        vae_config = yaml.safe_load(f)
+        config = LDMConfig(**vae_config["vae"])
+
+    ### Load Model and weights ###
+    model = VAE(config)
+    state_dict = load_file(path_to_pretrained_weights)
+    model.load_state_dict(state_dict, strict=True)
+    model.eval()
+    model = model.to(device)
+
+    ### Load Dataset ###
+    dataset, _ = get_dataset(dataset=dataset, path_to_data=path_to_dataset, num_channels=3, img_size=img_sz,
+                             random_resize=False, random_flip_p=0.0, train=False)
+    loader = DataLoader(dataset, batch_size=1, shuffle=False, drop_last=False,
+                        num_workers=8, pin_memory=False, persistent_workers=True)
+    samples = len(dataset)
+    print(f"found {samples} samples in {dataset}")
+
+    for batch in tqdm(loader):
+        with torch.no_grad():
+            img = batch["images"].to(device)  # (1, 3, img_h, img_w)
+
+            model_outputs = model(img)
+            reconstructions = model_outputs["reconstruction"]  # (batch, 3, H, W)
+
+            # Normalize to [0,1] for visualization
+            recon_disp = reconstructions.squeeze(0).permute(1, 2, 0).cpu().numpy()  # (h, w, 3)
+            recon_disp = (recon_disp - recon_disp.min()) / (recon_disp.max() - recon_disp.min() + 1e-8)
+
+            # Prepare the original image for display (1, 3, h, w) -> (h, w, 3)
+            img_disp = img.squeeze(0).permute(1, 2, 0).cpu().numpy()  # (h, w, 3)
+            img_disp = (img_disp - img_disp.min()) / (img_disp.max() - img_disp.min() + 1e-8)
+
+            # Side-by-side plot
+            plt.figure(figsize=(8, 4))
+            plt.subplot(1, 2, 1)
+            plt.imshow(img_disp)
+            plt.title("Original Image")
+            plt.axis("off")
+
+            plt.subplot(1, 2, 2)
+            plt.imshow(recon_disp)
+            plt.title("Recon Image")
+            plt.axis("off")
+
+            plt.tight_layout()
+            plt.show()
+            plt.close()
+
+
 if __name__ == "__main__":
-    visualize_latent(
+    # visualize_latent(
+    #     path_to_pretrained_weights='celeba256_SDVAE_bf16_b48_f8d4_flip/SDVAE/checkpoint_930000/model.safetensors',
+    #     config_file='configs/ldm_f8d4.yaml', dataset='celeba256', img_sz=256,
+    #     path_to_dataset='/home/mang/Downloads/celeba256/celeba256_visual',
+    # )
+
+    visualize_recon(
         path_to_pretrained_weights='celeba256_SDVAE_bf16_b48_f8d4_flip/SDVAE/checkpoint_930000/model.safetensors',
         config_file='configs/ldm_f8d4.yaml', dataset='celeba256', img_sz=256,
         path_to_dataset='/home/mang/Downloads/celeba256/celeba256_visual',
